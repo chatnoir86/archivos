b479642e7808bcf9f427748b918769da0df4658e
Usagi86 <Usagi86@users.noreply.github.com>
2015-08-07 00:33:14 -0600

    Update crear_nick_v1.0.js
    afrefado al arbol html para mostrar pop up para cambiarl el nick.

--- crear_nick_v1.0.js
+++ crear_nick_v1.0.js
@@ -1,3 +1,4 @@
+//v1 (2015/08/07)
 // crear elementos
 var nuevaAnclaCrear = document.createElement('a');
 var anclaTextoCrear = document.createTextNode('');
@@ -5,7 +6,7 @@ nuevaAnclaCrear.appendChild(anclaTextoCrear);
 var anclaEx = document.getElementById('sBTNS');
 var padre = anclaEx.parentNode;
 var nuevoHijoCrear = padre.insertBefore(nuevaAnclaCrear, anclaEx);
-nuevaAnclaCrear.innerHTML = '<div class='head' id='cambiar' style='visibility: hidden; position: absolute; top: -80px; left: 40px; border: 5px solid rgb(0, 0, 0); width: 200px; height: 100px; background: #000000'><a style='color: #FFFFFF'>Nickname:</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><button style='cursor: pointer; color: #FF0000' id='cierra_cambiar' title='cerrar este pop up' onmouseup='cerrar();'>X</button><div><a><input id='nuevo_nick'></input></a></div><div>&nbsp;</div><div><a>&nbsp;</a><a>&nbsp;</a><button style='cursor: pointer;' title='entrar&cambiar nick' id='btn_cambiar' class='btn' onmouseup='entrar();'>join Chat</button></div></div>';
+nuevaAnclaCrear.innerHTML = "<div id='cambiar' style='visibility: hidden; position: absolute; top: -80px; left: 40px; border: 5px solid rgb(255, 255, 255); width: 200px; height: 100px; background: #000000'><a style='color: #FFFFFF'>Nickname:</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><a>&nbsp;</a><button style='cursor: pointer; color: #FF0000' id='cierra_cambiar' title='cerrar este pop up' onmouseup='cerrar();'>X</button><div><a>&nbsp;</a><a><input id='nuevo_nick'></input></a></div><div>&nbsp;</div><div><a>&nbsp;</a><a>&nbsp;</a><button style='cursor: pointer;' title='entrar&cambiar nick' id='btn_cambiar' class='btn' onmouseup='entrar();'>join Chat</button></div></div>";
 
 
 // funciones para mostrar u ocultar
