c85d314309e1e533cb66e3e134fed0222046e5e1
Usagi86 <Usagi86@users.noreply.github.com>
2016-11-05 23:40:52 -0600

    Update nuevo.js

--- nuevo.js
+++ nuevo.js
@@ -2,4 +2,15 @@
 
 //alert("Hola. Esto es un mensaje de Texto! :)");
 
-onload()
+
+var x = document.body.getElementsByTagName("style");
+if (x.length)
+    x[0].parentElement.removeChild(x[0]);
+
+var newSS=document.createElement('link');
+newSS.rel='stylesheet';
+newSS.href='https://rawgit.com/Usagi86/ClienteLT/master/css_v6.css';
+document.getElementsByTagName("head")[0].appendChild(newSS);
+
+
+
