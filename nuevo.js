ae2ca35b8db97c77e8b90b26f8ef7017ad0fb1a6
Usagi86 <Usagi86@users.noreply.github.com>
2016-11-05 23:09:36 -0600

    Update nuevo.js

--- nuevo.js
+++ nuevo.js
@@ -2,7 +2,4 @@
 
 //alert("Hola. Esto es un mensaje de Texto! :)");
 
-var cabe = IN.CI.document.head
-var scriptt = document.createElement('script');
-scriptt.src="https://latinlatin.000webhostapp.com/nuevoj.js";
-cabe.appendChild(scriptt);
+onload()
